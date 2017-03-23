9a8caeb448549ee695c99df38c55a08520b635c7
KolinLoures <kolin-1994@mail.ru>
2017-03-24 02:12:37 +0300

    add all layouts

--- app/src/main/java/com/example/kolin/testya/MainActivity.java
+++ app/src/main/java/com/example/kolin/testya/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.kolin.testya;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.kolin.testya.adapter.ViewPagerAdapter;
 
@@ -20,9 +21,9 @@ public class MainActivity extends AppCompatActivity {
         viewPager = (NonSwipeViewPager) findViewById(R.id.main_view_pager);
 
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), false);
-        adapter.addFragment(TranslatorFragment.newInstance("",""), null);
-        adapter.addFragment(HistoryFavoriteFragment.newInstance("",""), null);
-        adapter.addFragment(InfoFragment.newInstance("",""), null);
+        adapter.addFragment(TranslatorFragment.newInstance("", ""), null);
+        adapter.addFragment(HistoryFavoriteFragment.newInstance("", ""), null);
+        adapter.addFragment(InfoFragment.newInstance("", ""), null);
 
         viewPager.setAdapter(adapter);
         viewPager.setPagingEnable(false);
@@ -34,31 +35,41 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                getSupportFragmentManager().popBackStack();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     //TODO: substitute first tab icon
     @SuppressWarnings("all")
     private void setupTabIcons() {
-            tabLayout.getTabAt(0).setIcon(R.drawable.ic_translate_black_24dp);
-            tabLayout.getTabAt(1).setIcon(R.drawable.ic_bookmark_black_24dp);
-            tabLayout.getTabAt(2).setIcon(R.drawable.ic_settings_black_24dp);
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_translate_black_24dp);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_bookmark_black_24dp);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_settings_black_24dp);
 
-            tabLayout.getTabAt(1).getIcon().setAlpha(128);
-            tabLayout.getTabAt(2).getIcon().setAlpha(128);
+        tabLayout.getTabAt(1).getIcon().setAlpha(128);
+        tabLayout.getTabAt(2).getIcon().setAlpha(128);
 
-            tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-                @Override
-                public void onTabSelected(TabLayout.Tab tab) {
-                    tab.getIcon().setAlpha(255);
-                }
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                tab.getIcon().setAlpha(255);
+            }
 
-                @Override
-                public void onTabUnselected(TabLayout.Tab tab) {
-                    tab.getIcon().setAlpha(128);
-                }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                tab.getIcon().setAlpha(128);
+            }
 
-                @Override
-                public void onTabReselected(TabLayout.Tab tab) {}
-            });
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
     }
 }
