11f48698ca656560a8c0b4a7ee140b0b701025f5
KolinLoures <kolin-1994@mail.ru>
2017-03-29 00:05:41 +0300

    add netSingleton - connection

--- app/src/main/java/com/example/kolin/testya/net/ITranslator.java
+++ app/src/main/java/com/example/kolin/testya/net/ITranslator.java
@@ -1,5 +1,8 @@
 package com.example.kolin.testya.net;
 
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+
 /**
  * RestApi for retrieve translation from Server
  */
@@ -10,6 +13,12 @@ public interface ITranslator {
             "trnsl.1.1.20170328T192532Z.584959536bdd55f3.8847edda17b6557aede1111bb2f0cf2c8a42c1ed";
     String BASE_URL_TRNSL =
             "https://translate.yandex.net/api/v1.5/tr.json/translate";
+
+    String API_KEY_DICT =
+            "dict.1.1.20170328T203832Z.a4dc04d938fbd1fd.75d3dd48e9f7a9e49dc1b4059f6f05a742480058";
+    String BASE_URL_DICT =
+            "https://dictionary.yandex.net/api/v1/dicservice";
+
 
 
 
--- app/src/main/java/com/example/kolin/testya/net/NetSingleton.java
+++ app/src/main/java/com/example/kolin/testya/net/NetSingleton.java
@@ -2,14 +2,15 @@ package com.example.kolin.testya.net;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by kolin on 28.03.2017.
  */
 
 public class NetSingleton {
-
-
 
     private NetSingleton netSingleton = null;
 
@@ -24,5 +25,23 @@ public class NetSingleton {
                 .build();
     }
 
+    private Retrofit provideRetrofit(){
+        return new Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(provideOkHttpClient())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    private ITranslator provideTranslator(){
+        return provideRetrofit().create(ITranslator.class);
+    }
+
+    public NetSingleton providenetSingleton(){
+        if (netSingleton == null){
+            netSingleton = new NetSingleton();
+        }
+        return netSingleton;
+    }
 
 }
