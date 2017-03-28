1fbe9f22822347945b914613f0634abb84894861
KolinLoures <kolin-1994@mail.ru>
2017-03-28 23:35:11 +0300

    add translation class - model

--- app/src/main/java/com/example/kolin/testya/net/ITranslator.java
+++ app/src/main/java/com/example/kolin/testya/net/ITranslator.java
@@ -6,9 +6,9 @@ package com.example.kolin.testya.net;
 
 public interface ITranslator {
 
-    String API_KEY_TRSL =
+    String API_KEY_TRNSL =
             "trnsl.1.1.20170328T192532Z.584959536bdd55f3.8847edda17b6557aede1111bb2f0cf2c8a42c1ed";
-    String BASE_URL_TRSL =
+    String BASE_URL_TRNSL =
             "https://translate.yandex.net/api/v1.5/tr.json/translate";
 
 
